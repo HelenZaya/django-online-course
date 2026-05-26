fc448cf29177d4cc665d01b7130423a12987c7fd
HelenZaya <mrs.buyanzaya@gmail.com>
2026-05-26 13:22:26 +0800

    Fix views.py to match sample: extract_answers, set choices, exam_result_bootstrap

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -24,36 +24,39 @@ def course_details(request, course_id):
     return render(request, 'onlinecourse/course_details_bootstrap.html', {'course': course})
 
 
-def submit(request, course_id):
-    user = request.user
-    course = get_object_or_404(Course, pk=course_id)
-    enrollment = get_object_or_404(Enrollment, user=user, course=course)
-    submission = Submission(enrollment=enrollment)
-    submission.save()
-    for key, value in request.POST.items():
+def extract_answers(request):
+    submitted_anwers = []
+    for key in request.POST:
         if key.startswith('choice'):
-            try:
-                selected_choice = Choice.objects.get(pk=int(value))
-                submission.choices.add(selected_choice)
-                submission.save()
-            except Choice.DoesNotExist:
-                pass
+            value = request.POST[key]
+            choice_id = int(value)
+            submitted_anwers.append(choice_id)
+    return submitted_anwers
+
+
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+    enrollment = Enrollment.objects.get(user=user, course=course)
+    submission = Submission.objects.create(enrollment=enrollment)
+    choices = extract_answers(request)
+    submission.choices.set(choices)
+    submission_id = submission.id
     return HttpResponseRedirect(
-        reverse('onlinecourse:show_exam_result', args=(course.id, submission.id))
+        reverse(viewname='onlinecourse:show_exam_result', args=(course_id, submission_id,))
     )
 
 
 def show_exam_result(request, course_id, submission_id):
+    context = {}
     course = get_object_or_404(Course, pk=course_id)
-    submission = get_object_or_404(Submission, pk=submission_id)
+    submission = Submission.objects.get(id=submission_id)
     choices = submission.choices.all()
-    selected_ids = choices.values_list('id', flat=True)
     total_score = 0
-    for question in course.question_set.all():
-        if question.is_get_score(selected_ids):
-            total_score += question.grade
-    return render(request, 'onlinecourse/exam_result.html', {
-        'course': course,
-        'submission': submission,
-        'total_score': total_score,
-    })
+    for choice in choices:
+        if choice.is_correct:
+            total_score += choice.question.grade
+    context['course'] = course
+    context['grade'] = total_score
+    context['choices'] = choices
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
