c51448a4ebee3aaf2c6b8fa1615f599b3ad228fa
HelenZaya <mrs.buyanzaya@gmail.com>
2026-05-26 13:14:09 +0800

    Fix admin.py to match sample: import Submission, fix inlines

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,20 +1,5 @@
 from django.contrib import admin
-from .models import Course, Lesson, Instructor, Learner, Enrollment, Question, Choice
-
-
-class ChoiceInline(admin.StackedInline):
-    model = Choice
-    extra = 4
-
-
-class QuestionInline(admin.StackedInline):
-    model = Question
-    extra = 5
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
-    list_display = ['content', 'course', 'grade']
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 
 class LessonInline(admin.StackedInline):
@@ -22,19 +7,36 @@ class LessonInline(admin.StackedInline):
     extra = 5
 
 
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title', 'course', 'order']
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
 
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline, QuestionInline]
-    list_display = ['name', 'pub_date', 'total_enrollment']
+    inlines = [LessonInline]
+    list_display = ['name', 'pub_date']
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
 
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Enrollment)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+admin.site.register(Submission)
